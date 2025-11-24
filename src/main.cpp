f7018c76cb9572f263be485e2a4908dfd01f22dc
msh31 <180540729+msh31@users.noreply.github.com>
2025-11-24 20:41:14 +0100

    fix: wrong return val

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 
-#include "node/node.hpp"
+#include "node.hpp"
+#include "tree.hpp"
 
 int main() {
     auto node = std::make_unique<Node>(42);
 
     if(node->left == nullptr && node->right == nullptr) {
         std::cout << "both children of a node are nullptrs\n";
-        return 1;
+        return 0;
     }
 
     std::cout << "left: " << node->left << "\n";
